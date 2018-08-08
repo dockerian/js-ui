3e512f825932a19bac0009191ffebe08e7ba01d6
Jason Zhu <jason.zhuyx@gmail.com>
2018-08-07 21:56:04 -0700

    updated test/setup.js to fix document.createRange TypeError

--- vue/test/unit/setup.js
+++ vue/test/unit/setup.js
@@ -1,3 +1,14 @@
 import Vue from 'vue'
 
 Vue.config.productionTip = false
+
+if (global.document) {
+  document.createRange = () => ({
+    setStart: () => {},
+    setEnd: () => {},
+    commonAncestorContainer: {
+      nodeName: 'BODY',
+      ownerDocument: document
+    }
+  })
+}
